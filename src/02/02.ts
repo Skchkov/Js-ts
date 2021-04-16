fbeb28d40071dbd7cbdf32db56051c5a336c8c81
Skchkov <skchkov@yandex.ru>
2021-04-16 17:22:23 +0300

    Added test 02

---
+++ src/02/02.ts
@@ -0,0 +1,47 @@
+type LocalCityType = {
+  title: string;
+  countryTitle: string;
+};
+
+type AdressType = {
+  streetTitle: string;
+  city: LocalCityType;
+};
+
+type TechType = {
+  id: number;
+  title: string;
+};
+
+type StudentType = {
+  id: number;
+  name: string;
+  age: number;
+  isActive: boolean;
+  adress: AdressType;
+  technologies: Array<TechType>;
+};
+
+const student: StudentType = {
+  id: 1,
+  name: "Dimych",
+  age: 32,
+  isActive: false,
+  adress: {
+    streetTitle: "Belskogo 53",
+    city: {
+      title: "Minks",
+      countryTitle: "Belarus",
+    },
+  },
+  technologies: [
+    { id: 1, title: "HTML" },
+    { id: 2, title: "CSS" },
+    { id: 3, title: "React" },
+  ],
+};
+
+console.log(student.age);
+console.log(student.name);
+console.log(student.adress.city.title);
+console.log(student.technologies[2].title);
