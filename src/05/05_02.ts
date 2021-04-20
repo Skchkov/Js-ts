89549a5b84dd411763965dc8d2bf521cbffd721b
Skchkov <skchkov@yandex.ru>
2021-04-20 14:17:56 +0300

    05 map and tests

---
+++ src/05/05_02.ts
@@ -0,0 +1,14 @@
+import { GovermentBuildingsType, HouseType } from "../02/02_02";
+
+export const getStreetsTitlesOfGovermentsBuildings = (
+  buildings: Array<GovermentBuildingsType>
+) => {
+  return buildings.map((b) => b.address.street.title);
+};
+export const getStreetsTitlesOfHouses = (houses: Array<HouseType>) => {
+  return houses.map((h) => h.address.street.title);
+};
+
+export const createMessages = (houses: Array<HouseType>) => {
+  return houses.map((h) => "Hello guys from " + h.address.street.title);
+};
--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,20 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  const names = ["Dimych", "Sveta", "Katya", "Viktor", "Ignat"];
+  const users = [
+    { name: "Dimych" },
+    { name: "Sveta" },
+    { name: "Katya" },
+    { name: "Viktor" },
+    { name: "Ignat" },
+  ];
+  const liElements = users.map((u, i) => <li key={i}>{u.name}</li>);
+  return (
+    <div className="App">
+      <ul>{liElements}</ul>
+    </div>
+  );
 }
 
 export default App;
